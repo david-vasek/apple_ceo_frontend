71ecb5852a3632b04fb685026345d29da8796072
David Vasek <david.vasek51@gmail.com>
2021-07-27 14:19:27 -0500

    created components folder

---
+++ src/components/CeoList.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import { Route, Link, useHistory } from "react-router-dom";
+import CeoDetailsAsync from "./CeoDetailsAsync";
+
+const CeoList = ({ reload }) => {
+  const [ceos, setCeos] = useState([]);
+  const history = useHistory();
+
+  useEffect(() => {
+    (async () => {
+      const ceoData = await fetch("http://127.0.0.1:3000/ceos").then(
+        (response) => response.json()
+      );
+      setCeos(ceoData);
+    })();
+  }, [reload]);
+
+  return (
+    <>
+      {!!ceos.length ? (
+        <>
+          <Route exact path="/">
+            <ul>
+              {ceos.map((ceo, index) => {
+                return (
+                  <li key={index}>
+                    <Link to={`/ceo/${ceo.slug}`}>{ceo.name}</Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </Route>
+          <Route path="/ceo/:ceo_slug">
+            <CeoDetailsAsync />
+            <button type="button" onClick={() => history.goBack()}>
+              Go Back
+            </button>
+          </Route>
+        </>
+      ) : (
+        <p>Loading CEOs...</p>
+      )}
+    </>
+  );
+};
+
+export default CeoList;
---
+++ src/components/StyledList.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const StyledList = styled.ul`
+  list-style: none;
+  font-size: 2rem;
+  text-align: left;
+`;
+
+export default StyledList;
